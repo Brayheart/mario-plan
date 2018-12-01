58db9d127fee5c70661267b475c1edbda680bc97
Tyler Bray <broncobray77@gmail.com>
2018-12-01 11:51:16 -0800

    project summary component renders

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,15 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
-  return <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Card Title</span>
-          <p>Posted By Tyler</p>
-          <p className="grey-text">Card Date</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Card Title</span>
-          <p>Posted By Tyler</p>
-          <p className="grey-text">Card Date</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Card Title</span>
-          <p>Posted By Tyler</p>
-          <p className="grey-text">Card Date</p>
-        </div>
-      </div>
-    </div>;
+  return (
+    <div className="project-list section">
+      <ProjectSummary/>
+      <ProjectSummary/>
+      <ProjectSummary/>
+      <ProjectSummary/>
+    </div>
+  )
 }
 
 export default ProjectList;
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Card Title</span>
+          <p>Posted By Tyler</p>
+          <p className="grey-text">Card Date</p>
+        </div>
+      </div>
+  )
+}
+
+export default ProjectSummary
